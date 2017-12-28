ddda445028a6d04b891507c9025b08ba09d71206
Ryan Kuester <rkuester@insymbols.com>
2017-12-27 21:57:18 -0600

    ptuxrepo: add test for bum DB permissions

--- ptuxrepo.d/tests/test_init.py
+++ ptuxrepo.d/tests/test_init.py
@@ -10,8 +10,15 @@ class TestInitEmpty(fixture.InEmptyRepo):
         self.assertTrue(os.path.isfile('dists/master/main/binary-amd64/Packages'))
 
     def testPermissions(self):
-        'has setgit and group-writeability'
+        'Created repository has setgit and group-writeability'
 
         mask = 0775 + stat.S_ISGID
         st = os.stat('dists/master/main')
+        self.assertEqual(st.st_mode & mask, mask)
+
+    def testDbPermissions(self):
+        'DB has group-writeability'
+
+        mask = 0o664
+        st = os.stat('.ptuxrepo/db/CURRENT')
         self.assertEqual(st.st_mode & mask, mask)
